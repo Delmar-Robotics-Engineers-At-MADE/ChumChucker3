ed84d854ff897e1c52d4c02b305e04d078aaf67b
LNV-T560\gallu <gallusgallus6@gmail.com>
2023-08-21 10:19:34 -0500

    Intake and changes.

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Intake extends SubsystemBase {
+
+    private final WPI_TalonFX motor = new WPI_TalonFX(Constants.Intake.ID);
+
+    Intake() {
+        motor.setNeutralMode(NeutralMode.Brake);
+    }
+    
+    public void run(double power) {
+        motor.set(ControlMode.PercentOutput, power);
+    }
+}
+
